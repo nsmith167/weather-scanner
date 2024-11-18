dd7c90d8825e8d2aafef0136a92b47b2d805ece0
Nathan Smith <smithfam@nathans-mbp.lan>
2024-11-17 21:06:46 -0500

    Remove defaults and add timer

--- scanner.py
+++ scanner.py
@@ -3,6 +3,7 @@ from weather_publisher import publish_weather
 import os
 import logging
 import sys
+import time
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -15,13 +16,17 @@ logger.addHandler(handler)
 
 api_key = os.getenv('WEATHER_API_KEY')
 location = f'{os.getenv("WEATHER_ZIP", "19460")} US'
-mqtt_host = os.getenv('MQTT_HOST', 'homepi3')
-mqtt_username = os.getenv('MQTT_USERNAME', 'home-metrics')
-mqtt_password = os.getenv('MQTT_PASSWORD', 'woodlawn')
+mqtt_host = os.getenv('MQTT_HOST')
+mqtt_username = os.getenv('MQTT_USERNAME')
+mqtt_password = os.getenv('MQTT_PASSWORD')
 weather_topic = os.getenv('WEATHER_TOPIC')
 
-try:
-    weather_data = get_weather(location, api_key)
-    publish_weather(mqtt_host, weather_topic, mqtt_username, mqtt_password, weather_data)
-except Exception as err:
-    logging.error(err)
+while(True):
+    try:
+        weather_data = get_weather(location, api_key)
+        publish_weather(mqtt_host, weather_topic, mqtt_username, mqtt_password, weather_data)
+    except Exception as err:
+        logging.error(err)
+
+    # Get weather data once every 5 seconds
+    time.sleep(5)
